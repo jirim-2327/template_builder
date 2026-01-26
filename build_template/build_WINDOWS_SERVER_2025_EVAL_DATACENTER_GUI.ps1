e6d8622612d91dd8b860b596bced6f9c998b5788
jirim-2327 <52720510+jehnevvfell@users.noreply.github.com>
2026-01-26 02:23:32 +0100

    wrappers changed as no_prompt is now disabled by default

--- build_template/build_WINDOWS_SERVER_2025_EVAL_DATACENTER_GUI.ps1
+++ build_template/build_WINDOWS_SERVER_2025_EVAL_DATACENTER_GUI.ps1
@@ -9,6 +9,6 @@ $buildPipelineScript = $(Join-Path $pipelines "Build-WindowsTemplate.ps1")
     -IsoId "WINDOWS_SERVER_2025_EVAL" `
     -ImageOption "Windows Server 2025 Datacenter Evaluation (Desktop Experience)" `
     -OverwriteDownloadedIso $false `
-    -CompareChecksums $false `
+    -CompareChecksums $true `
     -Use_No_Prompt_Iso $false `
     -OverwriteNoPromptIso $false
